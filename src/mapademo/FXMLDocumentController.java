5a753003a33a49212a845d8d1ac38e3e9b030aa6
Lunati23 <michael.riv2304@gmail.com>
2026-05-20 01:24:35 +0200

    Revert "Actividad Branch"

--- src/mapademo/FXMLDocumentController.java
+++ src/mapademo/FXMLDocumentController.java
@@ -76,7 +76,7 @@ import javafx.util.Duration;
  * Implementa {@link Initializable} para poder ejecutar código de
  * inicialización una vez que el FXML ha sido cargado completamente.
  */
-public class VisualizarActividadController implements Initializable {
+public class FXMLDocumentController implements Initializable {
 
     // =========================================================
     //  ESTRUCTURA DE NODOS PARA ZOOM
